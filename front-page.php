dd083bc25108da90433d2561927810ff54fafa7d
yura212137 <104753546+yura212137@users.noreply.github.com>
2022-05-10 22:40:03 +0900

    initial commit
    ハンバーガーサイトWP化

---
+++ front-page.php
@@ -0,0 +1,79 @@
+
+<?php get_header() ;?>
+        <!-- main -->
+        <main>
+          <!-- .p-fv -->
+          <section class="p-fv">
+            <h1 class="p-fv__title">
+              ダミーサイト
+            </h1>
+          </section>
+          <!-- .p-fv -->
+
+          <!-- .p-model -->
+          <section class="p-model">
+            <div class="p-model__takeout">
+              <h2 class="p-model__title c-title--takeout">
+                Take Out
+              </h2>
+              <div class="p-model__content">
+                <div class="p-model__description">
+                  <h3>Take OUT</h3>
+                  <p>当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
+                    で利用できる商品を掲載しています当店の
+                  </p>
+                </div>
+                <div class="p-model__description">
+                  <h3>Take OUT</h3>
+                  <p>当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
+                    で利用できる商品を掲載しています当店の
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="p-model__eatin">
+              <h2 class="p-model__title c-title">
+                Eat In
+              </h2>
+              <div class="p-model__content">
+                <div class="p-model__description">
+                  <h3>Eat In</h3>
+                  <p>店内でお食事いただけるメニューです店内でお食事いただけるメニューです<br>
+                    店内でお食事いただけるメニューです店内でお食事いただけるメニューです<br>
+                    店内でお食事いただけるメニューです店内でお食事いただけるメニューです<br>
+                    店内でお食事いただけるメニューです店内でお食事いただけるメニューです</p>
+                </div>
+                <div class="p-model__description">
+                  <h3>Eat In</h3>
+                  <p>店内でお食事いただけるメニューです店内でお食事いただけるメニューです<br>
+                    店内でお食事いただけるメニューです店内でお食事いただけるメニューです<br>
+                    店内でお食事いただけるメニューです店内でお食事いただけるメニューです<br>
+                    店内でお食事いただけるメニューです店内でお食事いただけるメニューです</p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <!-- .p-model -->
+
+          <!-- .p-access -->
+          <section class="p-access">
+            <div class="p-access__map"></div>
+              <div class="p-access__content">
+                <div class="p-access__description">
+                  <h2 class="p-access__title c-title--access">
+                    見出しが入ります
+                  </h2>
+                  <p class="p-access__text">
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                  </p>
+                </div>
+            </div>
+          </section>
+          <!-- .p-access -->  
+        </main>
+      </div>
+      <!-- main -->
+<?php get_sidebar() ;?>
+<?php get_footer() ;?>
+
+
---
+++ sidebar.php
@@ -0,0 +1,41 @@
+      <!-- aside -->
+      <aside class="l-aside">
+        <label for="menu" class="close">×</label>
+        <div class="l-aside__menu">
+          <a href="">Menu</a>
+        </div>
+        <ul class="l-aside__burger">
+          <li><a href="">バーガー</a></li>
+          <ul>
+            <li><a href="">ハンバーガー</a></li>
+            <li><a href="">チーズバーガー</a></li>
+            <li><a href="">テリヤキバーガー</a></li>
+            <li><a href="">アボカドバーガー</a></li>
+            <li><a href="">フィッシュバーガー</a></li>
+            <li><a href="">ベーコンバーガー</a></li>
+            <li><a href="">チキンバーガー</a></li>
+          </ul>
+        </ul>
+        <ul class="l-aside__side">
+          <li><a href="">サイド</a></li>
+          <ul>
+            <li><a href="">ポテト</a></li>
+            <li><a href="">サラダ</a></li>
+            <li><a href="">ナゲット</a></li>
+            <li><a href="">コーン</a></li>
+          </ul>
+        </ul>
+        <ul class="drink">
+          <li><a href="">ドリンク</a></li>
+          <ul>
+            <li><a href="">コーラ</a></li>
+            <li><a href="">ファンタ</a></li>
+            <li><a href="">オレンジ</a></li>
+            <li><a href="">アップル</a></li>
+            <li><a href="">紅茶（Ice/Hot）</a></li>
+            <li><a href="">コーヒー（Ice/Hot）</a></li>
+          </ul>
+        </ul>
+      </aside>
+    </div>
+    <!-- aside -->
